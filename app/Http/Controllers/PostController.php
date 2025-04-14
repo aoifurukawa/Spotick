4de810c89d45382026b75731b5170c23d73f66fa
aoifurukawa <furukawaaoi0728@gmail.com>
2025-04-14 13:13:34 +0900

    added bacend of post(inserting information)

---
+++ app/Http/Controllers/PostController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\Post;
+use Illuminate\Http\Request;
+
+class PostController extends Controller
+{
+    private $post;
+
+    public function __construct(Post $post)
+    {
+        $this->post = $post;
+    }
+
+    public function store(Request $request, $id)
+    {
+        $request->validate([
+
+            'title' => 'required|string|max:255',
+            'content' => 'required|integer', // 文章なら text に直したほうがいいかも
+            'description' => 'required|string',
+            'date' => 'required|date',
+            'price' => 'required|integer|min:0',
+            'number_of_tickets' => 'required|integer|min:1',
+            'venue' => 'required|string|max:255',
+            'address' => 'required|string|max:255',
+            'picture_1' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048', // ファイルアップロードなら別の対応が必要
+            'picture_2' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'picture_3' => 'required|image|mimes:jpeg,png,jpg,gif|max:2048',
+            'sponsor_name' => 'required|string|max:255',
+            'background_color' => 'required|string',
+            'mail_address' => 'required|email',
+            'insta_url' => 'nullable|url',
+            'facebook_url' => 'nullable|url',
+            'x_url' => 'nullable|url',
+        ]);
+
+        $this->post->user_id = $id;
+        $this->post->title = $request->title;
+        $this->post->content = $request->content;
+        $this->post->description = $request->description;
+        $this->post->date = $request->date;
+        $this->post->price = $request->price;
+        $this->post->number_of_tickets = $request->number_of_tickets;
+        $this->post->venue = $request->venue;
+        $this->post->address = $request->address;
+
+        if ($request->hasFile('picture_1')) {
+            $file = $request->file('picture_1');
+            $this->post->picture_1 = 'data:image/'.$file->extension().';base64,'.base64_encode(file_get_contents($file));
+        }
+
+        if ($request->hasFile('picture_2')) {
+            $file = $request->file('picture_2');
+            $this->post->picture_2 = 'data:image/'.$file->extension().';base64,'.base64_encode(file_get_contents($file));
+        }
+
+        if ($request->hasFile('picture_3')) {
+            $file = $request->file('picture_3');
+            $this->post->picture_3 = 'data:image/'.$file->extension().';base64,'.base64_encode(file_get_contents($file));
+        }
+
+        $this->post->sponsor_name = $request->sponsor_name;
+        $this->post->background_color = $request->background_color;
+        $this->post->mail_address = $request->mail_address;
+        $this->post->insta_url = $request->insta_url;
+        $this->post->facebook_url = $request->facebook_url;
+        $this->post->x_url = $request->x_url;
+
+        $this->post->save();
+
+        return 'information saving success';
+
+    }
+}
--- resources/views/sponsor/post.blade.php
+++ resources/views/sponsor/post.blade.php
@@ -4,7 +4,7 @@
 
 @section('content')
 <body style="background: url('{{ asset('images/cheering-picture.avif') }}') no-repeat center center fixed; background-size: cover;">
-    <form action="" method="post" enctype="multipart/form-data">
+    <form action="{{ route('post.store', Auth::user()->id) }}" method="post" enctype="multipart/form-data">
     @csrf
         <div class="container mt-5">
             <div class="card w-75 mx-auto" style="opacity: 0.95;">
@@ -17,80 +17,129 @@
                         <div class="col-8">
                             <label for="title" class='form-label fw-bold'>Title of event</label>
                             <input type="text" name="title" id="title" class="form-control">
+                            @error('title')
+                                <div class="text-danger">{{$message}}</div>
+                            @enderror
                         </div>
+                        
                         <div class="col-4">
                             <label for="content" class="form-label fw-bold">Content</label>
-                            <select name="" id="content" class="form-select">
-                                <option value=""></option>
-                                <option value="">match</option>
-                                <option value="">Fan festival</option>
+                            <select name="content" id="content" class="form-select">
+                                <option value="" hidden></option>
+                                <option value="1">match</option>
+                                <option value="2">Fan festival</option>
                             </select>
+                            @error('content')
+                                <div class="text-danger">{{$message}}</div>
+                            @enderror
                         </div>
                     </div>
 
                     <label for="description" class="form-label fw-bold mt-3">Description</label>
                     <textarea name="description" id="description" class="form-control"></textarea>
+                    @error('description')
+                        <div class="text-danger">{{$message}}</div>
+                    @enderror
                     <div class="row">
                         <div class="col-4">
                             <label for="date" class="form-label fw-bold mt-3">Date</label>
                             <input type="date" name="date" id="date" class="form-control">
+                            @error('date')
+                                <div class="text-danger">{{$message}}</div>
+                            @enderror
                         </div>
+                        
                         <div class="col-4">
                             <label for="price" class="form-label fw-bold mt-3">Price</label>
                             <input type="number" name="price" id="price" class="form-control" placeholder="$">
+                            @error('price')
+                                <div class="text-danger">{{$message}}</div>
+                            @enderror
                         </div>
+                        
                         <div class="col-4">
                             <label for="number" class="form-label fw-bold mt-3">Number of tickets</label>
-                            <input type="number" name="number" id="number" class="form-control">
+                            <input type="number" name="number_of_tickets" id="number" class="form-control">
+                            @error('number_of_tickets')
+                                <div class="text-danger">{{$message}}</div>
+                            @enderror
                         </div>
+                        
                     </div>
 
                     <label for="venue" class='form-label fw-bold mt-3'>Venue</label>
                     <input type="text" name="venue" id="venue" class="form-control">
+                    @error('venue')
+                        <div class="text-danger">{{$message}}</div>
+                    @enderror
 
-                    <label for="adress" class='form-label fw-bold mt-3'>Adress</label>
-                    <input type="text" name="adress" id="adress" class="form-control">
+                    <label for="address" class='form-label fw-bold mt-3'>Adress</label>
+                    <input type="text" name="address" id="address" class="form-control">
+                    @error('address')
+                        <div class="text-danger">{{$message}}</div>
+                    @enderror
 
                     <div class="row">
                         <div class="col-4">
                             <label for="picture" class="form-label fw-bold mt-3">Picture 1</label>
                             <input class="form-control mb-3" type="file" id="formFile" name="picture_1">
+                            @error('picture_1')
+                                <div class="text-danger">{{$message}}</div>
+                            @enderror
                         </div>
+                        
                         <div class="col-4">
                             <label for="picture" class="form-label fw-bold mt-3">Picture 2</label>
                             <input class="form-control mb-3" type="file" id="formFile" name="picture_2">
+                            @error('picture_2')
+                                <div class="text-danger">{{$message}}</div>
+                            @enderror
                         </div>
+                        
                         <div class="col-4">
                             <label for="picture" class="form-label fw-bold mt-3">Picture 3</label>
                             <input class="form-control mb-3" type="file" id="formFile" name="picture_3">
+                            @error('picture_3')
+                                <div class="text-danger">{{$message}}</div>
+                            @enderror
                         </div>
+                       
                     </div>
 
                     <div class="row">
                         <div class="col-4">
-                            <label for="background-color" class="form-label fw-bold mt-3">Background-color</label>
-                            <select name="background-color" id="background-color" class="form-select">
+                            <label for="background-color" class="form-label fw-bold mt-3">Background Color</label>
+                            <select name="background_color" id="background-color" class="form-select">
                                 <option value="" hidden>Grey</option>
-                                <option value="">red</option>
-                                <option value="">blue</option>
+                                <option value="red">red</option>
+                                <option value="blue">blue</option>
                             </select>
                         </div>
+                        @error('background_color')
+                            <div class="text-danger">{{$message}}</div>
+                        @enderror
                     </div>
 
-                    <label for="your-name" class="form-label fw-bold mt-3">Your Name</label>
-                    <input type="text" name="name" id="your-name" class="form-control">
+                    <label for="sponsor_name" class="form-label fw-bold mt-3">Your Name</label>
+                    <input type="text" name="sponsor_name" id="sponsor_name" class="form-control">
+                    @error('sponsor_name')
+                        <div class="text-danger">{{$message}}</div>
+                    @enderror
 
                     <label for="email" class="form-label fw-bold mt-3">Mail Adress</label>
-                    <input type="email" name="email" id="email" class="form-control">
+                    <input type="email" name="mail_address" id="email" class="form-control">
+                    @error('mail_address')
+                        <div class="text-danger">{{$message}}</div>
+                    @enderror
 
                     <label for="insta" class="form-label fw-bold mt-3">Instagram Account URL</label>
-                    <input type="url" name="insta" id="insta" class="form-control">
+                    <input type="url" name="insta_url" id="insta" class="form-control">
 
                     <label for="facebook" class="form-label fw-bold mt-3">FaceBook Account URL</label>
-                    <input type="url" name="facebook" id="facebook" class="form-control">
+                    <input type="url" name="facebook_url" id="facebook" class="form-control">
 
                     <label for="X" class="form-label fw-bold mt-3">X Account URL</label>
-                    <input type="url" name="X" id="X" class="form-control">
+                    <input type="url" name="x_url" id="X" class="form-control">
                 </div>
             </div>
 
